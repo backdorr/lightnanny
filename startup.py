f64db5014eda806cb3fdd4918c6a5ef86c7691a8
Brett Dorr <backdorr@gmail.com>
2013-10-06 20:29:38 +0000

    Updated startup code to include config file

--- startup.py
+++ startup.py
@@ -1,20 +1,33 @@
 #!/usr/bin/python
 
+config = {}
+execfile("startup.cfg", config)
+
 import smtplib
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.connect(('google.com', 0))
+ipaddress = s.getsockname()[0]
 
-sender = 'from@fromdomain.com'
-receivers = ['to@todomain.com']
+sender = config["gmail_user"]
+password = config["gmail_pass"]
+receivers = config["notification_recipients"]
 
-message = """From: From Person <from@fromdomain.com>
-To: To Person <to@todomain.com>
-Subject: SMTP e-mail test
+message = """From: 
+To: Light Nanny Recipients
+Subject: Light Nanny Startup
 
-This is a test e-mail message.
-"""
+The Light Nanny has been rebooted and has the local ip address of %s
+""" % ipaddress
 
 try:
-   smtpObj = smtplib.SMTP('localhost')
-   smtpObj.sendmail(sender, receivers, message)         
+   smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+   smtpObj.ehlo()
+   smtpObj.starttls()
+   smtpObj.ehlo()
+   smtpObj.login(sender, password)
+   smtpObj.sendmail(sender, receivers, message)
+   smtpObj.close()
    print "Successfully sent email"
-except SMTPException:
+except smtplib.SMTPException:
    print "Error: unable to send email"
